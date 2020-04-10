82bdd99f94474acddc7622a2d26706fb87fd18a9
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2020-04-10 08:46:51 -0300

    add post query

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 module Types
   class PostType < Types::BaseObject
+    field :id, ID, null: false
     field :title, String, null: true
     field :rating, Integer, null: true
     field :comments, [Types::CommentType], null: true
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,13 @@
+# frozen_string_literal: true
+
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    field :post, Types::PostType, null: true do
+      argument :id, ID, required: true
+    end
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    def post(id:)
+      Post.find(id)
     end
   end
 end
